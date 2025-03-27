940bee5eca77129a990ea4c67161bf23e90414c6
Ilario23 <ilario.zamberlan@gmail.com>
2025-03-27 23:22:50 +0100

    placed profile stuff in the right place, in profile

--- src/pages/profile/index.vue
+++ src/pages/profile/index.vue
@@ -1,23 +1,44 @@
 <script setup lang="ts">
-import router from '@/router';
+import {ref, computed} from 'vue';
 import {useUserStore} from '@/stores';
+import {showNotify} from 'vant';
 import defaultAvatar from '@/assets/images/default-avatar.svg';
+import {showConfirmDialog} from 'vant';
+
+const {t} = useI18n();
+const router = useRouter();
 
 const userStore = useUserStore();
 const userInfo = computed(() => userStore.userInfo);
-const isLogin = computed(() => !!userInfo.value.uid);
 
-function login() {
-  if (isLogin.value) return;
+// Stato per la dialog
+const showDialog = ref(false);
 
-  router.push({name: 'login', query: {redirect: 'profile'}});
+function clickProfile() {
+  showDialog.value = true; // Mostra la dialog
+}
+
+async function logout() {
+  showConfirmDialog({
+    title: t('profile.comfirmTitle'),
+  })
+    .then(() => {
+      showDialog.value = false; // Chiudi la dialog
+      showNotify({type: 'success', message: t('profile.succesfulLogout')});
+      userStore.logout();
+      router.push({name: 'login'});
+    })
+    .catch((error) => {
+      console.error('Errore durante il logout:', error);
+      showNotify({type: 'danger', message: t('profile.errorLogout')});
+    });
 }
 </script>
 
 <template>
   <div>
     <VanCellGroup :inset="true">
-      <van-cell center :is-link="!isLogin" @click="login">
+      <van-cell center @click="clickProfile">
         <template #title>
           <van-image
             :src="userInfo.avatar || defaultAvatar"
@@ -27,8 +48,7 @@ function login() {
         </template>
 
         <template #value>
-          <span v-if="isLogin">{{ userInfo.email }}</span>
-          <span v-else>{{ $t('profile.login') }}</span>
+          <span>{{ userInfo.email }}</span>
         </template>
       </van-cell>
     </VanCellGroup>
@@ -47,6 +67,49 @@ function login() {
         url="https://tenor.com/view/zeb-zeb89-eh-volevi-unto-gif-10755033"
       />
     </VanCellGroup>
+
+    <!-- Dialog -->
+    <van-dialog
+      v-model:show="showDialog"
+      :title="$t('profile.userInfo')"
+      confirm-button-text="Logout"
+      closeOnClickOverlay
+    >
+      <van-cell-group>
+        <div class="px-5">
+          <van-field
+            :label="$t('profile.userEmail')"
+            :placeholder="userInfo.email"
+            readonly
+            class="mt-10"
+          />
+          <van-field
+            :label="$t('profile.userDisplayName')"
+            :placeholder="userInfo.displayName"
+            readonly
+          />
+          <van-field
+            :label="$t('profile.userUid')"
+            :placeholder="userInfo.uid"
+            readonly
+          />
+          <van-cell center :title="$t('profile.userAvatar')">
+            <template #right-icon>
+              <van-image
+                :src="userInfo.avatar || defaultAvatar"
+                round
+                class="h-36 w-36"
+              />
+            </template>
+          </van-cell>
+        </div>
+      </van-cell-group>
+      <template #footer>
+        <van-button type="danger" @click="logout" block>
+          {{ $t('profile.logout') }}
+        </van-button>
+      </template>
+    </van-dialog>
   </div>
 </template>
 
@@ -54,7 +117,7 @@ function login() {
 {
   name: 'profile',
   meta: {
-    title: '个人中心',
+    title: 'profile',
     i18n: 'menus.profile',
   },
 }
--- src/pages/settings/index.vue
+++ src/pages/settings/index.vue
@@ -2,64 +2,46 @@
   <VanCellGroup :title="t('menus.basicSettings')" :border="false" :inset="true">
     <VanCell center :title="t('menus.darkMode')">
       <template #right-icon>
-        <VanSwitch v-model="checked" size="20px" aria-label="on/off Dark Mode" @click="toggle()" />
+        <VanSwitch
+          v-model="checked"
+          size="20px"
+          label="on/off Dark Mode"
+          @click="toggle()"
+        />
       </template>
     </VanCell>
 
-    <VanCell center is-link :title="t('menus.language')" :value="language" @click="showLanguagePicker = true" />
+    <VanCell
+      center
+      is-link
+      :title="t('menus.language')"
+      :value="language"
+      @click="showLanguagePicker = true"
+    />
 
     <VanCell center :title="$t('settings.currentVersion')">
-    
-      <div class="mt-10 text-gray">
-       v{{ version }}
-    </div>
-
+      <div class="text-gray">v{{ version }}</div>
     </VanCell>
-
-
   </VanCellGroup>
 
-
-
-
-  <!-- Van Field per l'email -->
-
-  <VanCellGroup :title="t('menus.user')" v-if="userInfo.email">
-  <div class="text-center">
-    <van-field label="E-mail" :placeholder="userInfo.email" readonly />
-
-    <!-- Tasto Esci con una classe custom -->
-    <van-button 
-      type="danger" 
-      size="large" 
-      @click="Logout">
-      {{ $t('settings.logout') }}
-    </van-button>
-  </div>
-</VanCellGroup>
-
-
-
   <van-popup v-model:show="showLanguagePicker" position="bottom">
-    <van-picker v-model="languageValues" :columns="languageColumns" @confirm="onLanguageConfirm"
-      @cancel="showLanguagePicker = false" />
+    <van-picker
+      v-model="languageValues"
+      :columns="languageColumns"
+      @confirm="onLanguageConfirm"
+      @cancel="showLanguagePicker = false"
+    />
   </van-popup>
 </template>
 
 <script setup lang="ts">
-import type { PickerColumn } from 'vant';
-import { showConfirmDialog } from 'vant';
-import router from '@/router';
-import { useUserStore } from '@/stores';
-import { version } from '~root/package.json';
+import type {PickerColumn} from 'vant';
+import {version} from '~root/package.json';
 import useAppStore from '@/stores/modules/app';
-import { languageColumns, locale } from '@/utils/i18n';
+import {languageColumns, locale} from '@/utils/i18n';
 
-
-const { t } = useI18n();
-const userStore = useUserStore();
+const {t} = useI18n();
 const appStore = useAppStore();
-const userInfo = computed(() => userStore.userInfo);
 const checked = ref<boolean>(isDark.value);
 
 watch(
@@ -67,7 +49,7 @@ watch(
   (newMode) => {
     checked.value = newMode;
   },
-  { immediate: true }
+  {immediate: true}
 );
 
 function toggle() {
@@ -75,40 +57,19 @@ function toggle() {
   appStore.switchMode(isDark.value ? 'dark' : 'light');
 }
 
-
-
 const showLanguagePicker = ref(false);
 const languageValues = ref<Array<string>>([locale.value]);
 const language = computed(
   () => languageColumns.find((l) => l.value === locale.value).text
 );
 
-function onLanguageConfirm(event: { selectedOptions: PickerColumn }) {
+function onLanguageConfirm(event: {selectedOptions: PickerColumn}) {
   locale.value = event.selectedOptions[0].value as string;
   showLanguagePicker.value = false;
 }
-
-function Logout() {
-  showConfirmDialog({
-    title: t('settings.comfirmTitle'),
-  })
-    .then(() => {
-      userStore.logout();
-      router.push({ name: 'login' });
-    })
-    .catch(() => { });
-}
 </script>
 
-
-
-<style scoped>
-.van-text-color {
-  --van-cell-text-color: var(--van-red);
-}
-
-
-</style>
+<style scoped></style>
 
 <route lang="json5">
 {
