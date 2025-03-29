a298b2b9d994a6fb67f7e2000dd829c24f5ffff9
Ilario23 <ilario.zamberlan@gmail.com>
2025-03-29 02:16:54 +0100

    giochini con le icone

--- src/pages/unocss/index.vue
+++ src/pages/unocss/index.vue
@@ -1,23 +1,23 @@
 <template>
-  <h1 class="text-6xl color-pink font-semibold">
-    Hello, Unocss!
-  </h1>
+  <h1 class="text-6xl color-pink font-semibold">Hello, Unocss!</h1>
 
   <p class="mt-4 text-gray-700 dark:text-white">
     This is a simple example of Unocss in action.
   </p>
 
-  <button class="btn">
-    Button
-  </button>
+  <button class="btn">Button</button>
+
+  <div class="w-50 h-50 i-mdi-heart bg-blue"></div>
 </template>
+
+<script setup lang="ts"></script>
 
 <route lang="json5">
 {
   name: 'unocss',
   meta: {
     title: '🎨 Unocss',
-    i18n: 'menus.unocssExample'
+    i18n: 'menus.unocssExample',
   },
 }
 </route>
