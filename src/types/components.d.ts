afe854091bf97f2620d515867a2b5973d0d61a34
Ilario23 <ilario.zamberlan@gmail.com>
2025-04-09 02:20:05 +0200

    update dependencies

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -30,7 +30,6 @@ declare module 'vue' {
     VanIcon: typeof import('vant/es')['Icon']
     VanImage: typeof import('vant/es')['Image']
     VanNavBar: typeof import('vant/es')['NavBar']
-    VanNumberKeyboard: typeof import('vant/es')['NumberKeyboard']
     VanPicker: typeof import('vant/es')['Picker']
     VanPopup: typeof import('vant/es')['Popup']
     VanPullRefresh: typeof import('vant/es')['PullRefresh']
