8b8cd703a666d8528f6656ec92370ca50fcafcb6
Ilario23 <ilario.zamberlan@gmail.com>
2025-04-20 02:27:07 +0200

    workaround build di vercel

---
+++ scripts/vercel-build.js
@@ -0,0 +1,11 @@
+const {execSync} = require('child_process');
+
+const isProd = process.env.VERCEL_ENV === 'production';
+
+const command = isProd ? 'pnpm run build' : 'pnpm run build:dev';
+
+console.log(
+  `🛠️  Running build: ${command} (VERCEL_ENV=${process.env.VERCEL_ENV})`
+);
+
+execSync(command, {stdio: 'inherit'});
