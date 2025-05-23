46c76555964c35fa7f0f7760a5db21899a5547f7
Ilario23 <ilario.zamberlan@gmail.com>
2025-05-23 15:16:18 +0200

    pulizia

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,6 @@ export default ({mode}: ConfigEnv): UserConfig => {
     server: {
       host: true,
       port: 3000,
-      allowedHosts: ['.ngrok-free.app'],
       proxy: {
         '/api': {
           target: '',
